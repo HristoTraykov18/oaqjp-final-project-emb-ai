0e02a4482852265aea94c5eca96256de959bf7d7
Hristo Traykov <hristotraykov98@gmail.com>
2024-08-06 14:35:54 +0300

    Added error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,8 +8,19 @@ def emotion_detector(text_to_analyse):
     header = {
         "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json=input_json, headers=header)
-    formatted_response = json.loads(response.text)
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
-    emotions['dominant_emotion'] = max(emotions, key=emotions.get)
+
+    if response.status_code == 400:
+        emotions = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+    else:
+        formatted_response = json.loads(response.text)
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        emotions['dominant_emotion'] = max(emotions, key=emotions.get)
 
     return emotions
--- server.py
+++ server.py
@@ -6,19 +6,24 @@ app = Flask(__name__)
 
 @app.route("/emotionDetector")
 def detector():
-    if request.args.get('textToAnalyze'):
+    if request.args.get('textToAnalyze') is not None:
         emotions = emotion_detector(request.args.get('textToAnalyze'))
         dominant_emotion = emotions['dominant_emotion']
-        del emotions['dominant_emotion']
-        outp = 'For the given statement, the system response is '
 
-        for emotion in emotions.keys():
-            if emotion != list(emotions)[-1]:
-                outp += f"'{emotion}': {emotions[emotion]}, "
-            else:
-                outp += f"and '{emotion}': {emotions[emotion]}. "
+        if dominant_emotion:
+            del emotions['dominant_emotion']
+            outp = 'For the given statement, the system response is '
 
-        outp += f'The dominant emotion is <strong>{dominant_emotion}</strong>.'
+            for emotion in emotions.keys():
+                if emotion != list(emotions)[-1]:
+                    outp += f"'{emotion}': {emotions[emotion]}, "
+                else:
+                    outp = outp[:-2] + \
+                        f" and '{emotion}': {emotions[emotion]}. "
+
+            outp += f'The dominant emotion is <strong>{dominant_emotion}</strong>.'
+        else:
+            outp = '<strong>Invalid text! Please try again!</strong>'
 
         return outp
     else:
